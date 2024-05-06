6c088f81cde7e345bc5473c7807b7c4ec0c41698
jaskomalsingh <jsinghma@uwo.ca>
2024-05-06 12:32:27 -0400

    pre-phone-deploy

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';root
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-
-
-);
-
-*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
-
